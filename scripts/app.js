eb0b14efa09f3c85067a6311f150e417cb98cad0
Eden Porte <eden.porte@iut2.univ-grenoble-alpes.fr>
2021-04-21 16:24:30 +0200

    docs utiles + maj instagram

--- scripts/app.js
+++ scripts/app.js
@@ -21,7 +21,7 @@ vm.component('myheader', {
                     <li><a href="#">L'accueil de loisirs</a></li>
                     <li><a href="#"><pre>   - Mercredis</pre></a></li>
                     <li><a href="#"><pre>   - Les vacances</pre></a></li>
-                    <li><a href="#"><pre>   - Les séjours</pre></a></li>
+                    <li><a href="sejours.html"><pre>   - Les séjours</pre></a></li>
                     <li><a href="#">Le périscolaire</a></li>
                     <li><a href="#">L'accompagnement à la scolarité</a></li>
                 </ul>
@@ -50,10 +50,10 @@ vm.component('myheader', {
             <li>
                 <a href="sejours.html">Les séjours</a>
                 <ul class="sous">
-                    <li><a href="#">Vacances d'hiver</a></li>
-                    <li><a href="#">Vacances d'été</a></li>
-                    <li><a href="#">Vacances d'automne</a></li>
-                    <li><a href="#">Vacances de printemps</a></li>
+                    <li><a href="sejours.html#hiver">Vacances d'hiver</a></li>
+                    <li><a href="sejours.html#ete">Vacances d'été</a></li>
+                    <li><a href="sejours.html#automne">Vacances d'automne</a></li>
+                    <li><a href="sejours.html#printemps">Vacances de printemps</a></li>
                     <li><a href="#">Le projet enfance jeunesse</a></li>
                     <li><a href="#">Autres périodes</a></li>
                 </ul>
@@ -61,12 +61,12 @@ vm.component('myheader', {
             <li>
                 <a href="#">L'association MJC</a>
                 <ul class="sous">
-                    <li><a href="#">Les statuts</a></li>
-                    <li><a href="#">Le projet associatif</a></li>
-                    <li><a href="#">Le conseil d'administration</a></li>
-                    <li><a href="#">Organigrame</a></li>
-                    <li><a href="#">Le projet enfance jeunesse</a></li>
-                    <li><a href="#">Règlement intérieur de la MJC</a></li>
+                    <li><a href="#" onclick="window.open(this.href); return false;">Les statuts</a></li>
+                    <li><a href="#" onclick="window.open(this.href); return false;">Le projet associatif</a></li>
+                    <li><a href="#" onclick="window.open(this.href); return false;">Le conseil d'administration</a></li>
+                    <li><a href="#" onclick="window.open(this.href); return false;">Organigrame</a></li>
+                    <li><a href="#" onclick="window.open(this.href); return false;">Le projet enfance jeunesse</a></li>
+                    <li><a href="#" onclick="window.open(this.href); return false;">Règlement intérieur de la MJC</a></li>
                 </ul>
             </li>
             <li>
@@ -81,8 +81,9 @@ vm.component('myheader', {
             <li>
                 <a href="#">Documents utiles</a>
                 <ul class="sous">
-                    <li><a href="#">Fiche adhésion</a></li>
-                    <li><a href="#">Fiche sanitaire pour les enfants et jeunes</a></li>
+                    <li><a href="data/docs/fiche-adhesion.pdf" onclick="window.open(this.href); return false;">Fiche adhésion</a></li>
+                    <li><a href="data/docs/fiche-sanitaire.pdf" onclick="window.open(this.href); return false;">Fiche sanitaire pour les enfants et jeunes</a></li>
+                    <li><a href="data/docs/plaquette-annuelle.pdf" onclick="window.open(this.href); return false;">Plaquette anuelle</a></li>
                 </ul>
             </li>
             
@@ -103,6 +104,7 @@ vm.component('myfooter', {
             <h2>Contact</h2>
             <p>1 place de la commune de 1871,<br>38100 Grenoble</p>
             <p>Nous appeler :<br> 04 76 51 12 51</p>
+            <p>Mail : secretariat@mjc-abbaye.org</p>
         </div>
         <div>
             <h2>Horaires d'ouverture du secrétariat</h2>
@@ -113,8 +115,8 @@ vm.component('myfooter', {
         </div>
         <div>
             <h2>Suivez nous</h2>
-            <p><a href="https://fr-fr.facebook.com/MJCAbbayeGrenoble38"><img src="data/img/logo-facebook.png" alt="logo-facebook"></a></p>
-            <p><a href="#"><img src="data/img/logo-instagram.png" alt="logo-facebook"></a></p>
+            <p><a href="https://www.facebook.com/MJCAbbayeGrenoble38"><img src="data/img/logo-facebook.png" alt="logo-facebook"></a></p>
+            <p><a href="https://www.instagram.com/mjcabbayegrenoble/"><img src="data/img/logo-instagram.png" alt="logo-facebook"></a></p>
         </div>
     </footer>
     `
